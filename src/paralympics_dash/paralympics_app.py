53c6e1f7dfcee6e74262748f0f4a1401a16fd4b4
Darius <dariusjoshi@hotmail.co.uk>
2024-02-23 15:17:30 +0000

    Update requirements.txt and add Bootstrap styling to the Dash app

--- src/paralympics_dash/paralympics_app.py
+++ src/paralympics_dash/paralympics_app.py
@@ -1,10 +1,30 @@
 from dash import Dash, html
+import dash_bootstrap_components as dbc
 
-app = Dash(__name__)
+# Variable that contains the external_stylesheet to use, in this case Bootstrap styling from dash bootstrap components (dbc)
+external_stylesheets = [dbc.themes.DARKLY]
 
-app.layout = html.Div([
-    html.Div(children='Hello World')
+# Define a variable that contains the meta tags
+meta_tags = [
+    {"name": "viewport", "content": "width=device-width, initial-scale=1"},
+]
+
+# Pass the stylesheet variable to the Dash app constructor
+app = Dash(__name__, external_stylesheets=external_stylesheets)
+
+app.layout = dbc.Container(
+    html.Div([
+    html.H1('Hello Dash'),
+    html.Div([
+        html.P('Dash converts Python classes into HTML'),
+        html.P("This conversion happens behind the scenes by Dash's JavaScript front-end"),
+        html.Button('Submit', id='submit-val', n_clicks=0)
+    ]),
+    html.Div([
+        html.Img(src=app.get_asset_url('line-chart-placeholder.png'))
+    ])
 ])
+)
 
 if __name__ == '__main__':
     app.run(debug=True)
